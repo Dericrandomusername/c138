809c88a0a8f5876e2a893d3ff5d181a027bca4fe
Dericrandomusername <107108479+Dericrandomusername@users.noreply.github.com>
2023-05-10 16:27:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function bedroom(){window.location="bedroom.html"}
+function desk(){window.location="desk.html"}
+function fruitbasket(){window.location="fruitbasket.html"}
+function TVac(){window.location="livingroom.html"}
+function bottle(){window.location="water.html"}
---
+++ water.js
@@ -0,0 +1,38 @@
+function setup(){
+    canvas=createCanvas(640,420)
+    canvas.center()
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+    document.getElementById("status").innerHTML="status:detecting objects"
+}
+status="";
+objects=[];
+function modelLoaded(){
+    console.log("model Loaded");
+    status="true";
+    objectDetector.detect(img,gotResults)
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+
+    }
+    console.log(results);
+    objects=results
+}
+function preload(){
+    img=loadImage("water.jpg");
+}
+function draw(){
+    image(img,0,0,640,420);
+   if(status!=""){
+    for(var i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status: Object detected";
+        fill("white");
+         percent=floor(objects[i].confidence*100);
+         text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+         noFill ();
+         stroke("white");
+         rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+   }
+}
